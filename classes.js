6948e4a23f200539c043cb362a70f0c8a75d869e
Mateo Daud <mateodaud@gmail.com>
2023-10-26 20:10:10 -0300

    comienzo de transicion de logica a backend

---
+++ classes.js
@@ -0,0 +1,14 @@
+class Piece {
+    constructor(type, color) {
+        this.type = type
+        this.color = color
+    }
+}
+
+class Move {
+    constructor(piece, square) {
+
+    }
+}
+
+module.exports = { Piece, Move }
